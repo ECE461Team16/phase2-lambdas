ac09d8e5b77d4d9f45f350652c0fe31fd9e5201d
Calix-Mingyu-Kim <calixkim58@gmail.com>
2023-12-12 18:39:27 -0500

    update Function updated

--- update/app.ts
+++ update/app.ts
@@ -1,120 +1,133 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import AWS from 'aws-sdk';
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, UpdateCommand, GetCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, UpdateCommand, GetCommand } from '@aws-sdk/lib-dynamodb';
 
 const client = new DynamoDBClient({});
 const docClient = DynamoDBDocumentClient.from(client);
-const TableName = "registry"
+const TableName = 'registry';
 
 //TODO: Update score, Update S3 bucket
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  console.log(event);
-  const metadata = JSON.parse(event.body)?.metadata;
-  const data = JSON.parse(event.body)?.data;
-  const id = event.pathParameters?.id;
-  console.log(metadata)
-  console.log(data)
-  console.log('id: ', id);
+    console.log(event);
 
-  if (!id) {
-      return {
-          statusCode: 400,
-          body: JSON.stringify({
-              error: 'There is missing field(s) in the PackageID/AuthenticationToken or it is formed improperly, or the AuthenticationToken is invalid.',
-          }),
-      };
-  }
+    let metadata;
+    let data;
+    let id;
 
-  const getCommand = new GetCommand({
-    TableName: TableName,
-    Key: {
-      "id": metadata.ID,
-    },
-  });
+    if (event.body) {
+        metadata = JSON.parse(event.body)?.metadata;
+        data = JSON.parse(event.body)?.data;
+        console.log('metadata: ', metadata);
+        console.log('data: ', data);
+    } else {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                error: 'There is missing field(s) in the PackageID/AuthenticationToken or it is formed improperly, or the AuthenticationToken is invalid.',
+            }),
+        };
+    }
+    id = event.pathParameters?.id;
+    console.log('id: ', id);
 
-  
-  try {
-    const result = await docClient.send(getCommand);
-    // console.log(result.Item);
-    if (result.Item) {
-      // Update the item
-      let url = {"URL": data.URL};    
-      const Score = await calculateScore(url);  // Update Score in DynamoDB
-      console.log(Score);
+    if (!id) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                error: 'There is missing field(s) in the PackageID/AuthenticationToken or it is formed improperly, or the AuthenticationToken is invalid.',
+            }),
+        };
+    }
 
-      const binaryData: Buffer = Buffer.from(data.Content, 'base64');
-      const s3 = new AWS.S3();
-      const params = {
-          Bucket: 'ingested-package-storage',
-          Key: `${metadata.name}.zip`,
-          Body: binaryData,
-      };
-      await s3
-          .upload(params, (err: Error, data: any) => {
-              console.log(err, data);
-          })
-          .promise();
-      
-      const updateCommand = new UpdateCommand({
+    const getCommand = new GetCommand({
         TableName: TableName,
         Key: {
-          "id": metadata.ID,
+            id: metadata.ID,
         },
-        UpdateExpression: "set version = :version, ratings = :ratings",
-        ExpressionAttributeValues: {
-          ':version': metadata.Version,
-          ':ratings': JSON.stringify(Score.Score),
-        },
-      });
+    });
 
-      await docClient.send(updateCommand);  // Update Version and Content in DynamoDB
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ message: 'Version is updated' }),
-        headers: {
-          'Access-Control-Allow-Headers': 'Content-Type',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
-        },
-      };
-    } else {
-      console.log('Package does not exist');
-      return {
-        statusCode: 404,
-        body: JSON.stringify({ error: 'Package does not exist' }),
-      };
+    try {
+        const result = await docClient.send(getCommand);
+        // console.log(result.Item);
+        if (result.Item) {
+            // Update the item
+            let url = { URL: data.URL };
+            const Score = await calculateScore(url); // Update Score in DynamoDB
+            console.log(Score);
+
+            const binaryData: Buffer = Buffer.from(data.Content, 'base64');
+            const s3 = new AWS.S3();
+            const params = {
+                Bucket: 'ingested-package-storage',
+                Key: `${metadata.name}.zip`,
+                Body: binaryData,
+            };
+            await s3
+                .upload(params, (err: Error, data: any) => {
+                    console.log(err, data);
+                })
+                .promise();
+
+            const updateCommand = new UpdateCommand({
+                TableName: TableName,
+                Key: {
+                    id: metadata.ID,
+                },
+                UpdateExpression: 'set version = :version, ratings = :ratings',
+                ExpressionAttributeValues: {
+                    ':version': metadata.Version,
+                    ':ratings': JSON.stringify(Score.Score),
+                },
+            });
+
+            await docClient.send(updateCommand); // Update Version and Content in DynamoDB
+            return {
+                statusCode: 200,
+                body: JSON.stringify({ message: 'Version is updated' }),
+                headers: {
+                    'Access-Control-Allow-Headers': 'Content-Type',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
+                },
+            };
+        } else {
+            console.log('Package does not exist');
+            return {
+                statusCode: 404,
+                body: JSON.stringify({ error: 'Package does not exist' }),
+            };
+        }
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Internal server error' }),
+        };
     }
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Internal server error' }),
-    };
-  }
-}
+};
 
 async function calculateScore(url: any): Promise<{ Score: any }> {
-  const lambda = new AWS.Lambda();
+    const lambda = new AWS.Lambda();
     const params = {
-    FunctionName: 'RateFunction-RateFunction-tYak9soW0m0J',
-    InvocationType: 'RequestResponse',
-    // LogType: 'Tail',
-    Payload: JSON.stringify(url, null, 2)
+        FunctionName: 'RateFunction-RateFunction-tYak9soW0m0J',
+        InvocationType: 'RequestResponse',
+        // LogType: 'Tail',
+        Payload: JSON.stringify(url, null, 2),
     };
-    
+
     try {
-      const response = await lambda.invoke(params).promise();
-      // console.log('response:', response);
-      if (response.StatusCode !== 200) {
-        console.log('Error in lambda invoke', response);
-        return { Score: 0 };
-      }
-      // console.log('scores', JSON.parse(response.Payload as string).body.NET_SCORE);
-      const score = JSON.parse(response.Payload as string).body;
-      const { URL, ...scores } = score;
-      return { Score: scores };
+        const response = await lambda.invoke(params).promise();
+        // console.log('response:', response);
+        if (response.StatusCode !== 200) {
+            console.log('Error in lambda invoke', response);
+            return { Score: 0 };
+        }
+        // console.log('scores', JSON.parse(response.Payload as string).body.NET_SCORE);
+        const score = JSON.parse(response.Payload as string).body;
+        const { URL, ...scores } = score;
+        return { Score: scores };
     } catch (error) {
-      console.error(error);
-      return { Score: 0 };
+        console.error(error);
+        return { Score: 0 };
     }
 }
